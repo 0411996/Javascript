601c35582c4c9d06187346ec81e5956e5659af5a
0411996 <39014450+0411996@users.noreply.github.com>
2020-04-07 16:47:46 +0530

    Add files via upload
    A sampe program to sort numbers in an array according to their shortest digit sum , each number more than 1 digit is broken down to it's sum of digits till 1 digit remains and then sorted in ascending order

---
+++ Assignmntscript.js
@@ -0,0 +1,10 @@
+cappfrm-frm_load
+1)new document frm startup
+
+3)open any kind of document
+
+cdtsmenu
+1)new doc frm startup
+2)file->new doc
+
+For addendum api call needs to be in Form_Load in cappform.cs and fr,Authorinfo
